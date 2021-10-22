73234d61a94a7ce7ef70004d77901c820570caa6
aaalace <tyj5resd@gmail.com>
2021-10-23 00:05:08 +0300

    23.10.21 login + signin ui done

--- design files/signin.py
+++ design files/signin.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 
 
-class MyWidget(QMainWindow):
+class MyWidgetSignin(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -26,7 +26,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    form = MyWidget()
+    form = MyWidgetSignin()
     form.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
