0a2e81cf882bf8cd23be612f3a8679e625660e68
aaalace <tyj5resd@gmail.com>
2021-10-25 01:20:10 +0300

    25.10.21

--- windows/reviews_code.py
+++ windows/reviews_code.py
@@ -2,20 +2,20 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QSize
-from fillers import Ui_MainWindow
+from reviews import Ui_MainWindow
 
 
 class MyWidgetLogin(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.initUi()
+        self.initUI()
 
-    def initUi(self):
-        self.btn_clear.setIcon(QIcon('../static/trash.png'))
-        self.btn_clear.setIconSize(QSize(27, 27))
+    def initUI(self):
+        screen = app.primaryScreen()
+        size = screen.size()
+        self.list_items.move(40, 130)
+        self.list_items.resize(size.width() - 80, size.height() - 230)
 
 
 def except_hook(cls, exception, traceback):
@@ -25,6 +25,6 @@ def except_hook(cls, exception, traceback):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = MyWidgetLogin()
-    form.show()
+    form.showMaximized()
     sys.excepthook = except_hook
     sys.exit(app.exec())
