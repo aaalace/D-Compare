d5ee51a27e3ba179e6b9950eb6cc35de09450200
aaalace <tyj5resd@gmail.com>
2021-10-24 23:15:29 +0300

    23.10.21 login + signin ui done

---
+++ dialog files/home.py
@@ -0,0 +1,31 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import uic
+
+
+class MyWidgetLogin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('../design files/home.ui', self)
+        screen = app.primaryScreen()
+        size = screen.size()
+        print(size.width(), size.height())
+        self.list_items.move(30, int(size.height() / 5))
+        self.list_items.resize(int(size.width()) - 60, (int(size.height() / 1.4)))
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyWidgetLogin()
+    form.showMaximized()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
