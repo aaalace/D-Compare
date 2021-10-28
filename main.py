62d3c60341b146e5c27f300642468ac8a7b2b861
aaalace <tyj5resd@gmail.com>
2021-10-28 17:53:00 +0300

    28.10

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
+import sqlite3
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLabel, QLineEdit
 from PyQt5.QtCore import QSize
 from templates.forms.login import Login_Form
 from templates.forms.signin import Signin_Form
@@ -11,11 +12,16 @@ from templates.forms.main_wind import Main_Form
 from templates.forms.filters import Filters_Form
 
 
+con = sqlite3.connect("databases/main_db.sqlite")
+cur = con.cursor()
+
+
 class MyWidgetLogin(QMainWindow, Login_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.initUI()
+
         self.btn_register.clicked.connect(self.open_register)
         self.btn_entry.clicked.connect(self.check_login)
 
@@ -25,12 +31,15 @@ class MyWidgetLogin(QMainWindow, Login_Form):
         self.lbl_pic.setPixmap(self.pixmap)
         self.lbl_pic.resize(250, 75)
         self.lbl_pic.move(280, 55)
+        self.line_password.setEchoMode(QLineEdit.Password)
 
     def open_register(self):
         form_login.hide()
         form_signin.show()
 
     def check_login(self):
+        self.result = cur.execute('''SELECT * FROM users''').fetchall()
+        print(self.result)
         self.open_main()
 
     def open_main(self):
@@ -44,7 +53,8 @@ class MyWidgetSignin(QMainWindow, Signin_Form):
         self.setupUi(self)
         self.initUI()
         self.btn_login.clicked.connect(self.open_login)
-        self.btn_register.clicked.connect(self.check_register)
+        self.btn_register.clicked.connect(self.register)
+        self.line_password.setEchoMode(QLineEdit.Password)
 
     def initUI(self):
         self.lbl_pic = QLabel(self)
@@ -53,8 +63,8 @@ class MyWidgetSignin(QMainWindow, Signin_Form):
         self.lbl_pic.resize(250, 75)
         self.lbl_pic.move(280, 55)
 
-    def check_register(self):
-        pass
+    def register(self):
+        cur.execute('''INSERT INTO users(id, username, password) VALUES(2, "asd", "afe")''')
         self.open_login()
 
     def open_login(self):
