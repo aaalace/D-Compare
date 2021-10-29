74e32e9c1e33faeae3b138780e8afe4b7e255203
aaalace <tyj5resd@gmail.com>
2021-10-30 00:40:38 +0300

    29.10

--- home_util.py
+++ home_util.py
@@ -90,9 +90,9 @@ class MyWidgetFilters(QMainWindow, Filters_Form):
 
     def add_filters(self):
         self.box_producer.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
-        self.box_display_size.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
-        self.box_ghz.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
-        self.box_ram.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
-        self.box_matrix.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
-        self.box_base_camera.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
-        self.box_front_camera.addItems(['Все', 'Apple', 'Samsung', 'Xiaomi', 'Poco', 'OnePlus', 'Huawei'])
+        self.box_display_size.addItems(['Все', '<5', '5 - 5.5', '5.6 - 6', '6.1 - 6.3', '6.4 - 6.6', '>6.6'])
+        self.box_ghz.addItems(['Все', '90 Гц', '120 Гц', '144 Гц'])
+        self.box_ram.addItems(['Все', '<=2 ГБ', '3 ГБ', '4 ГБ', '6 ГБ', '8 ГБ', '>=12 ГБ'])
+        self.box_base_camera.addItems(['Все', '<8 Мп', '8 Мп', '12 - 20 Мп', '21 - 40 Мп', '>40 Мп'])
+        self.box_front_camera.addItems(['Все', '<2 Мп', '3 - 8 Мп', '13', '16', '>16 Мп'])
+        self.box_matrix.addItems(['Все', 'IPS', 'AMOLED', 'Super AMOLED', 'OLED'])
